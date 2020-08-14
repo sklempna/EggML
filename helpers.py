52a944d898afcd541175b98c6a37c060e13d9214
sklempna <60576241+sklempna@users.noreply.github.com>
2020-08-14 14:12:23 +0200

    expanded tabs into spaces

--- helpers.py
+++ helpers.py
@@ -3,23 +3,23 @@ import math
 # activation functions
 
 def logistic(x):
-	return 1/(1 + math.exp(-x))
+    return 1/(1 + math.exp(-x))
 
 def logistic_prime(x):
-	return math.exp(x)/(1 + math.exp(x))**2
+    return math.exp(x)/(1 + math.exp(x))**2
 
 def tanh(x):
-	return (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+    return (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
 
 def tanh_prime(x):
-	return 1 - tanh(x)**2
+    return 1 - tanh(x)**2
 
 def linear(x):
-	return x
+    return x
 
 def linear_prime(x):
-	return 1
+    return 1
 
 act_functions = {'logistic' : (logistic, logistic_prime),
-					'tanh' : (tanh, tanh_prime),
-					'linear' : (linear, linear_prime)}
+                    'tanh' : (tanh, tanh_prime),
+                    'linear' : (linear, linear_prime)}
